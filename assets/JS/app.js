e47001d48ac95da30c37a37f08f10934ed763158
nehadashraf <nehadashraf402@gmail.com>
2022-11-26 04:08:37 +0200

    first commit

---
+++ assets/JS/app.js
@@ -0,0 +1,12 @@
+wow = new WOW(
+    {
+    boxClass:     'animate animated',      // default
+  }
+  )
+  wow.init();
+  jQuery("#responsive_headline").fitText(1.2, { minFontSize: '20px', maxFontSize: '40px' });
+/* $(document).ready(function(){
+    $("#flip").click(function(){
+      $("#panel").slideDown("slow");
+    });
+  }); */
